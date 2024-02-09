fcc3d44251179199fcadf8f451e5d5cb51220e7c
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-09 17:57:45 -0500

    i believe the server.js file is basically structured the way it is supposed to be with the gets and middleware

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
+const api = require('./public/assets/index.js')
+
+const app = express();
+const PORT = 3001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET for notes
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
