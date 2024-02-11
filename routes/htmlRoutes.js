316fca5c82bf02b10ab22dbe9c2de00087f72f58
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-11 15:08:54 -0500

    added comments to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,15 @@
 const router = require('express').Router();
 const path = require('path');
 
+// routes to the homepage "index.html"
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// routes to the note page "notes.html"
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// makes available for use in other places(modules)
 module.exports = router;
