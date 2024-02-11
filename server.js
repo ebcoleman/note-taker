bed3b8fa226d954931546685aa0ea2a5af201c30
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-02-11 15:11:50 -0500

    added notes to server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+// starts server and listens for requests
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
